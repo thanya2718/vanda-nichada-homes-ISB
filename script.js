abdca9fac78e00fb4ea0e674ae8c169de7ffc7be
thanya2718 <2tlinkth@gmail.com>
2026-05-31 21:02:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+navToggle?.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+});
+
+document.querySelectorAll('.nav-links a').forEach((link) => {
+  link.addEventListener('click', () => navLinks.classList.remove('open'));
+});
